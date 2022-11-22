7acbb0068d33541a294245d9c6266210400f9a00
Noziho <noah.decroix3@gmail.com>
2022-11-22 12:25:00 +0100

    add Project objet + print

---
+++ assets/js/Project.js
@@ -0,0 +1,62 @@
+export function Project (name, thumbnail, prodLink, gitHubLink) {
+
+    this.name = name;
+    this.thumbnail = thumbnail;
+    this.prodLink = prodLink;
+    this.gitHubLink = gitHubLink;
+    this.containerProjects = document.querySelector('.projectContainer');
+
+
+    this.setProject = function () {
+        let containerProject = document.createElement("div");
+        containerProject.classList = "project padding-2 margin-inline-2 margin-top-4";
+
+        let projectNameContainer = document.createElement("div");
+
+        let projectName = document.createElement("h1");
+        projectName.innerHTML = this.name;
+
+
+
+        let projectThumbnailContainer = document.createElement("div");
+        projectThumbnailContainer.classList = "thumbnailContainer  margin-top-1";
+
+        let projectThumbnail = document.createElement("img");
+        projectThumbnail.src = this.thumbnail;
+        projectThumbnail.alt = "Project Image";
+        projectThumbnail.classList = "projectThumbnail";
+
+
+
+        let projectProdLinkContainer = document.createElement("div");
+        projectProdLinkContainer.classList = "margin-top-2";
+
+        let projectProdLink = document.createElement("a");
+        projectProdLink.innerHTML = "Lien de production"
+        projectProdLink.href = this.prodLink;
+        projectProdLink.add
+
+
+
+        let projectGitHubLinkContainer = document.createElement("div");
+        projectGitHubLinkContainer.classList = "margin-top-2";
+
+        let projectGitHubLink = document.createElement("a");
+        projectGitHubLink.innerHTML = "GitHub link";
+        projectGitHubLink.href = this.gitHubLink;
+
+
+
+        projectNameContainer.append(projectName);
+        projectThumbnailContainer.append(projectThumbnail);
+        projectProdLinkContainer.append(projectProdLink);
+        projectGitHubLinkContainer.append(projectGitHubLink);
+
+        containerProject.append(
+            projectNameContainer, projectThumbnailContainer, projectProdLinkContainer, projectGitHubLinkContainer
+        );
+
+        this.containerProjects.append(containerProject);
+
+    }
+}
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 import '../styles/styles.scss';
+import {Project} from "./Project";
+import gameSiteThumbnails from '../img/gameSiteThumbnail.png'
+
+let test1 = new Project('GameSite', gameSiteThumbnails, 'https://gamesite.noziho.com', '');
+test1.setProject();
+
+let test2 = new Project('test', 'tete', 'https://google.fr', 'https://github.com/Noziho/gameSite');
+test2.setProject();
+
+let test3 = new Project('test', 'tete', 'https://google.fr', 'https://moodle.up-to.fr/course/view.php?id=17&sectionid=814');
+test3.setProject();
+
+let test4 = new Project('test', 'tete', 'https://google.fr', 'https://moodle.up-to.fr/course/view.php?id=17&sectionid=814');
+test4.setProject();
+
+let test5 = new Project('test', 'tete', 'https://google.fr', 'https://moodle.up-to.fr/course/view.php?id=17&sectionid=814');
+test5.setProject();
+
+let test6 = new Project('test', 'tete', 'https://google.fr', 'https://moodle.up-to.fr/course/view.php?id=17&sectionid=814');
+test6.setProject();
+
